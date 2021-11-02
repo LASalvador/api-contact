c469fabff5a6f53a62f490300b1f40509a896379
Lucas Salvador <lucas.salvador11358@gmail.com>
2021-11-02 14:42:38 -0300

    added sorce code

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const cors = require('cors')
+const express = require('express')
+const emailService = require('./services/emailService.js')
+const app = express()
+require('dotenv/config');
+const port = process.env.PORT;
+app.use(express.json());
+app.use(cors())
+
+app.post('/', emailService.sendEmail)
+
+app.get('/', (req,res)=> {
+    res.send('API CONTACT-US')
+})
+
+app.listen(port, () =>{
+    console.log(`Servidor rodando na porta ${port}!`)
+});
---
+++ src/services/emailService.js
@@ -0,0 +1,52 @@
+const nodemailer = require('nodemailer');
+require('dotenv/config');
+const USER = process.env.EMAIL;
+const sender = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: 465,
+    secure: true,
+    auth: {
+        user: USER,
+        pass: process.env.PASSWORD
+    }
+});
+
+
+const sendEmail = async (req, res) => {
+    if (!validateEmail(req.body.to)) {
+        res.status(400).send('Email to is invalid')
+        return
+    }
+    
+    var mailOptions = {
+        from: USER,
+        to: req.body.to,
+        subject: req.body.subject,
+        html: createEmailMessage(req.body.name, req.body.email, req.body.message),
+    };
+    let info;
+    try {
+        info = await sender.sendMail(mailOptions);
+        emailMessage = "Message sent: " + info.messageId
+        console.log(emailMessage);
+        res.status(200).send(emailMessage)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error to send email')
+    }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+
+function createEmailMessage(name, email, message) {
+    return '<b>Name:</b>' + name + '<br><b>Email:</b>' + email + '<br><b>Message:</b>' + message;
+}
+
+module.exports = {
+    sendEmail
+}
+
